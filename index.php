7073b9ae7f8e47b93370c61f7a27fac4724e6074
AL72RU <gshurik88@gmail.com>
2022-09-28 23:44:29 +0500

    init

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>СПА процедуры</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="container">
+            <a class="navbar-brand">СПА процедуры</a>
+            <div class="navbar-wrap">
+                <ul class="navbar-menu">
+                    <li><a href="/login.php">Log in</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+	
+    <div class="row">
+        <div class="box1">
+            <img src="/images/spa.jpg">
+        </div>
+    </div>
+</body>
+</html>
